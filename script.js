ef3350d5e624702db5d59ded6fc788d933bc414b
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-13 19:44:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
-
+var one= document.getElementById('radius');
+var two=document.getElementById('volume');
+function volume_sphere()
+ {
+  var volume;
+  var radius =one.value;
+  radius = Math.abs(radius);
+  volume = (4/3) * Math.PI * Math.pow(radius, 3);
+  volume = volume.toFixed(4);
+  two.value = volume;
+  return false;
+ } 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
